1a2f8470e0c579349cc064c023edfd4105c58080
Danila <Bbunok991@yandex.ru>
2023-06-23 18:27:26 +0300

    Completed the task_19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,10 +1,45 @@
 ﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 14212 -> нет
 12821 -> да
-23432 -> да
+23432 -> да*/
+
+/*Random rand = new Random();
+string random;
+string number = "";
+string[] List = new string[5];
+for(int i = 0; i < List.Length; i++)
+{
+    random = Convert.ToString(rand.Next(0, 10));
+    List[i] = random;
+    number += List[i];
+}
+
+System.Console.WriteLine($"Пятизначное число: {number}");
+if(List[0] == List[4] && List[1] == List[3]) System.Console.WriteLine("Это число палиндром");
+else System.Console.WriteLine("Это число не палиндром");*/
+
+string InPut(string message)
+{
+    System.Console.WriteLine(message);
+    string number = System.Console.ReadLine();
+    return number;
+}
+
+string list = InPut("Enter a five-digit number: ");
+
+void Palindrome()
+{
+    if(list[0] == list[4] && list[1] == list[3]) System.Console.WriteLine("Это число палиндром");
+    else System.Console.WriteLine("Это число не палиндром");
+}
+Palindrome();
 
 
-Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
+
+
+/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
 
@@ -12,3 +47,28 @@ A (7,-5, 0); B (1,-1,9) -> 11.53
 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125*/
+
+
+
+
+
+/*int InputX(string message)
+{
+    System.Console.Write(message);
+    int X = Convert.ToInt32(Console.ReadLine());
+    return X;
+}
+
+int InputY(string message)
+{
+    System.Console.Write(message);
+    int Y = Convert.ToInt32(Console.ReadLine());
+    return Y;
+}
+
+int x = */
+
+
+
+
+
