192e7bff280547414d1012ca16d3d43aac170918
Danila <Bbunok991@yandex.ru>
2023-06-23 19:18:07 +0300

    Deleted comments

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -3,21 +3,6 @@
 12821 -> да
 23432 -> да*/
 
-/*Random rand = new Random();
-string random;
-string number = "";
-string[] List = new string[5];
-for(int i = 0; i < List.Length; i++)
-{
-    random = Convert.ToString(rand.Next(0, 10));
-    List[i] = random;
-    number += List[i];
-}
-
-System.Console.WriteLine($"Пятизначное число: {number}");
-if(List[0] == List[4] && List[1] == List[3]) System.Console.WriteLine("Это число палиндром");
-else System.Console.WriteLine("Это число не палиндром");*/
-
 string InPut(string message)
 {
     System.Console.WriteLine(message);
